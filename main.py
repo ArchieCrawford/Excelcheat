509688e996b359c983627d87e87324dbce87e1d8
archiecrawford1-6895 <archie.crawford1@gmail.com>
2026-03-02 20:45:26 -0500

    update

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 import io
 import openpyxl
 from docx import Document
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 TEMPLATE_PATH = "template.docx"
 RECEIVED_MARK = "X"
